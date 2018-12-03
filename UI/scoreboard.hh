633e19b4646af42e4f141ffc9594a9672360d99c
Samu Reilimo <samu.reilimo@student.tut.fi>
2018-12-03 14:46:01 +0200

    Alustava pisteenlaskujärjestelmä ja peliohjeet

---
+++ UI/scoreboard.hh
@@ -0,0 +1,15 @@
+#ifndef SCOREBOARD_HH
+#define SCOREBOARD_HH
+
+
+class scoreboard
+{
+public:
+    scoreboard();
+
+    void checkIsGameFinished();
+
+    void addPointsToPlayer();
+};
+
+#endif // SCOREBOARD_HH
